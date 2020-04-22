5b72feb621c259e2dcff68377f476b4dec6aecad
agbleze <62272559+agbleze@users.noreply.github.com>
2020-04-22 02:35:01 +0200

    Update new.js

--- new.js
+++ new.js
@@ -14,3 +14,14 @@ evenOddResult =  (even === 0) ?
 console.log ("Even Number") : console.log( "Odd Number");
 console.log(evenOddResult);
 
+// Final result should be 10.42
+// Add/update your code here
+
+let result = (5.21 + 5.21) / (1 + 1);
+let result2 = 2 / 10 * 10;
+
+result *= result2;
+finalResult = result.toFixed(2);
+typeof (finalResult);
+finalNumber = +finalResult
+
