1d3746e6cde09ea67b916456f04fea711ec8dbd7
agbleze <62272559+agbleze@users.noreply.github.com>
2020-04-22 02:05:39 +0200

    Create new.js

---
+++ new.js
@@ -0,0 +1,16 @@
+// Test your skills: Math 1 : https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Test_your_skills:_Math //
+
+let variableOne = 12;
+let variableTwo = 12;
+let variableThree = 4;
+let variableFour = 2;
+
+let variableFive = variableOne + variableTwo;
+let variableSix = variableThree - variableFour;
+
+finalResult = variableFive * variableSix;
+even = finalResult % 2 
+evenOddResult =  (even === 0) ?
+console.log ("Even Number") : console.log( "Odd Number");
+console.log(evenOddResult);
+
